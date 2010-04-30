9348dafe13d5b8f093c3ed2c58f37f70031cd2d9
Dennis Hedegaard <neo2k@neo2k.dk>
2010-04-30 17:47:00 +0200

    25 done in perl, some of 19.

---
+++ euler25.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl -w
+use strict;
+use warnings;
+use Math::BigInt;
+
+my ($i, $fn, $fn1, $fn2) = (1, Math::BigInt->new(1), Math::BigInt->new(1), Math::BigInt->new(0));
+my @count;
+while (@count < 1000) {
+        $fn = $fn1 + $fn2;
+        $fn2 = $fn1;
+        $fn1 = $fn;
+        $i++;
+	@count = split(//, $fn);
+}
+print $i . "\n";
