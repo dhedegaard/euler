2813cd8e84f74dbbdadc83f9296b19b4d25d80f6
Dennis Hedegaard <neo2k@neo2k.dk>
2010-04-30 18:25:34 +0200

    25: more correctly syntaxed.

--- euler25.pl
+++ euler25.pl
@@ -5,11 +5,11 @@ use Math::BigInt;
 
 my ($i, $fn, $fn1, $fn2) = (1, Math::BigInt->new(1), Math::BigInt->new(1), Math::BigInt->new(0));
 my @count;
-while (@count < 1000) {
+do {
 	$fn = $fn1 + $fn2;
 	$fn2 = $fn1;
 	$fn1 = $fn;
 	$i++;
 	@count = split(//, $fn);
-}
+} until (@count == 1000);
 print $fn . "\n" . $i . "\n";
