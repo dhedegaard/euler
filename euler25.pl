cfa6f81abcd360e18aab409cce2aba47503bc974
Dennis Hedegaard <neo2k@neo2k.dk>
2010-04-30 18:03:54 +0200

    25: removed spaces

--- euler25.pl
+++ euler25.pl
@@ -6,10 +6,10 @@ use Math::BigInt;
 my ($i, $fn, $fn1, $fn2) = (1, Math::BigInt->new(1), Math::BigInt->new(1), Math::BigInt->new(0));
 my @count;
 while (@count < 1000) {
-        $fn = $fn1 + $fn2;
-        $fn2 = $fn1;
-        $fn1 = $fn;
-        $i++;
+	$fn = $fn1 + $fn2;
+	$fn2 = $fn1;
+	$fn1 = $fn;
+	$i++;
 	@count = split(//, $fn);
 }
 print $fn . "\n" . $i . "\n";
