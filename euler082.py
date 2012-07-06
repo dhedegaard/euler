ab916e2579a8a53d4fbec90aa74d51da52484f7f
Dennis Hedegaard <neo2k@neo2k.dk>
2012-07-06 03:50:47 +0200

    euler 82 cleanup and refactoring

--- euler082.py
+++ euler082.py
@@ -2,14 +2,6 @@
 
 import sys
 
-data = """
-131,673,234,103,18
-201,96,342,965,150
-630,803,746,422,111
-537,699,497,121,956
-805,732,524,37,331
-""".strip()
-
 class From:
     class LEFT: pass
     class ABOVE: pass
@@ -70,30 +62,27 @@ def get_grid_data():
         con.close()
         return data.strip()
 
+def generate_cache(grid):
+    _cache = []
+    for _y in grid:
+        _cache_arr = []
+        for _x in _y:
+            _cache_arr.append(-1)
+        _cache.append(_cache_arr)
+    for _y in xrange(len(grid)):
+        _cache[_y][-1] = grid[_y][-1]
+    return _cache
+
 if __name__ == "__main__":
     grid = []
-    cache_above = []
-    cache_below = []
-    cache_left = []
     for _line in get_grid_data().strip().split("\n"):
         arr = []
-        cache_arr_above = []
-        cache_arr_below = []
-        cache_arr_left = []
         for _num in _line.split(","):
             arr.append(int(_num))
-            cache_arr_above.append(-1)
-            cache_arr_below.append(-1)
-            cache_arr_left.append(-1)
         grid.append(arr)
-        cache_above.append(cache_arr_above)
-        cache_below.append(cache_arr_below)
-        cache_left.append(cache_arr_left)
-    # put cache_above in for endpoints.
-    for _y in xrange(len(grid)):
-        cache_above[_y][-1] = grid[_y][-1]
-        cache_below[_y][-1] = grid[_y][-1]
-        cache_left[_y][-1] = grid[_y][-1]
+    cache_above = generate_cache(grid)
+    cache_below = generate_cache(grid)
+    cache_left = generate_cache(grid)
     _min = sys.maxint
     for y in xrange(len(grid)):
         _rc = min_path(grid, cache_above, cache_below, cache_left, y=y)
