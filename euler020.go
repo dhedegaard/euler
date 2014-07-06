253bd7b59c4f36cfc562b4902a4d0b92377e3667
Dennis Hedegaard <dennis@dhedegaard.dk>
2014-07-07 00:37:18 +0200

    added some comments to euler020.go

--- euler020.go
+++ euler020.go
@@ -5,10 +5,13 @@ import "fmt"
 import "strconv"
 
 func main() {
+	// calculate the faculty
 	bignum := big.NewInt(1)
 	for a := 100; a >= 1; a-- {
 		bignum.Mul(big.NewInt(int64(a)), bignum)
 	}
+
+	// calculate the sum of the digits in the big number.
 	sum := 0
 	for _, char := range bignum.String() {
 		digit, _ := strconv.ParseInt(string(char), 0, 0)
